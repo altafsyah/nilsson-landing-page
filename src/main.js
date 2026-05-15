ba95b6a74d4458748b97a8daf9a12af25d56a51c
altafsyah <syahrastanialtaf@gmail.com>
2026-05-15 20:15:57 +0700

    feat: add gsap and scroll behaviour on dekstop

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
 import "./css/main.css"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 
-console.log("init")
+gsap.registerPlugin(ScrollTrigger)
+
+function initHorizontalScroll() {
+  const mm = gsap.matchMedia()
+
+  mm.add("(min-width: 769px)", () => {
+    const wrapper = document.querySelector("#sections-wrapper")
+    const sections = gsap.utils.toArray("#sections-wrapper > section")
+    const totalScrollWidth = wrapper.scrollWidth - window.innerWidth
+
+    const snapPoints = sections.map(
+      (section) => section.offsetLeft / totalScrollWidth
+    )
+
+    gsap.to(wrapper, {
+      x: -totalScrollWidth,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#scroll-container",
+        pin: true,
+        scrub: 0.5,
+        snap: {
+          snapTo: snapPoints,
+          duration: { min: 0.2, max: 0.6 },
+          ease: "power1.inOut",
+        },
+        end: () => "+=" + totalScrollWidth,
+        invalidateOnRefresh: true,
+      },
+    })
+  })
+}
+
+window.addEventListener("load", initHorizontalScroll)
